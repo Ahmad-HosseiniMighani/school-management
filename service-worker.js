62993e75e8c316cf3bdbb8068eba6a2b215465a2
Ahmad <13ahmad.hm78@gmail.com>
2020-07-14 03:49:50 +0430

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/school-management/precache-manifest.1da0c9714a43aa99acfba2217c691b3a.js"
+  "/school-management/precache-manifest.b821e3545aef2c90d236e903c38cad7f.js"
 );
 
 self.addEventListener('message', (event) => {
